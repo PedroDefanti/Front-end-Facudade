20e4f681b57761a32a87ecad557753b6e8a8fed1
PedroDefanti <168847376+PedroDefanti@users.noreply.github.com>
2025-09-22 22:08:11 -0300

    condicionais

---
+++ revisao/script2.js
@@ -0,0 +1,30 @@
+let nota=5.5
+//condicional
+// if (nota>=6){
+//     console.log('Aprovado')
+// }else{
+//     console.log('Reprovado')
+// }
+
+// console.log(nota=='7')
+// console.log(nota==='7')
+// console.log(nota !=5)
+// console.log(nota >=7)
+
+// ternario
+// let situacao=nota>=6 ? 'aprovado': 'reprovado'
+
+// switch
+// switch(true){
+//     case nota >=6:
+//         console.log('Aprovado')
+//         break
+//     case nota <=6 && nota >= 4:
+//         console.log('recuperação')
+//         break
+//     case nota <4:
+//         console.log('Reprovado')
+//         break
+//     default:
+//         console.log('O aluno não fez a prova')
+// }
