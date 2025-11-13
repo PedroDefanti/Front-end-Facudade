607fdfaa098754bfd65157f6554fcb56f4345bf3
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2025-11-13 10:54:47 -0300

    Projeto  RPG

--- p2/aula_10/11/25/assests/JS/classes.js
+++ p2/aula_10/11/25/assests/JS/classes.js
@@ -3,18 +3,24 @@ class Character {
     maxlife = 1
     attack = 0
     defense = 0
+    type = ''
+    
 
     constructor(name) {
         this.name = name
     }
 
     get life() {
-        return this._life.toFixed(2)
-
+        return this._life // Retorna o número diretamente
     }
 
     set life(newLife) {
         this._life = newLife < 0 ? 0 : newLife
+    }
+    
+    // Método auxiliar para exibir a vida formatada
+    get lifeFormatted() {
+        return this._life.toFixed(2)
     }
 }
 
@@ -25,6 +31,8 @@ class Knight extends Character {
         this.attack = 10
         this.defense = 8
         this.maxlife = this.life
+        this.type = 'Hero'
+        this.image = 'heroi.png'
     }
 }
 class Sorcerer extends Character {
@@ -34,6 +42,8 @@ class Sorcerer extends Character {
         this.attack = 25
         this.defense = 5
         this.maxlife = this.life
+        this.type = 'Hero'
+        this.image = 'heroi.png'
     }
 }
 
@@ -45,29 +55,36 @@ class LittleMonster extends Character {
         this.attack = 4
         this.defense = 4
         this.maxlife = this.life
+        this.type = 'Monster'
+        this.image = 'monstro.jpg'
     }
 }
 
 class BigMonster extends Character {
     constructor() {
-        super('Little Monster')
+        super('Big Monster')
         this.life = 120
         this.attack = 16
         this.defense = 6
         this.maxlife = this.life
+        this.type = 'Monster'
+        this.image = 'monstro.png' 
     }
 }
 
 class Stage {
-    constructor(fighter1, fighter2, fighter1EL, fighter2EL,logObject) {
+    constructor(fighter1, fighter2, fighter1EL, fighter2EL, logObject) {
         this.fighter1 = fighter1
         this.fighter2 = fighter2
         this.fighter1EL = fighter1EL
         this.fighter2EL = fighter2EL
-        this.log=logObject
+        this.log = logObject
+        this.turn = fighter1
     }
+    
     start() {
-        this.update()
+        this.update() 
+        
         this.fighter1EL.querySelector('.attackbutton').addEventListener("click", () => {
             this.doAttack(this.fighter1, this.fighter2)
         })
@@ -75,66 +92,110 @@ class Stage {
             this.doAttack(this.fighter2, this.fighter1)
         })
     }
+    
     doAttack(attacking, attacked) {
-        // verificação base
-        if (attacking.life <= 0 || attacked.life <= 0) {
-            this.log.addMessage('VC está atacando um cachorro morto')
+        if (attacking !== this.turn) {
+            this.log.addMessage(`**SISTEMA**: Não é o turno de **${attacking.name}**!`, 'system-msg')
+            return
         }
-        let attackFactor = (Math.random() * 2).toFixed(2)
 
-        let defenseFactor = (Math.random() * 2).toFixed(2)
+        if (attacking.life <= 0) {
+            this.log.addMessage(`**SISTEMA**: **${attacking.name}** está morto e não pode atacar.`, 'system-msg')
+            this.turn = attacked 
+            this.update()
+            return
+        }
+
+        if (attacked.life <= 0) {
+            this.log.addMessage(`**SISTEMA**: **${attacking.name}** está atacando um cachorro morto (**${attacked.name}**).`, 'system-msg')
+            this.turn = attacking 
+            this.update()
+            return
+        }
+
+        let attackFactor = Math.random() * 2 
+        let defenseFactor = Math.random() * 2 
 
         let actualAttack = attacking.attack * attackFactor
+        let actualDefense = attacked.defense * defenseFactor 
 
-        let actualDefense = attacking.defense * defenseFactor
+        let msgClass = attacking.type === 'Hero' ? 'hero-msg' : 'monster-msg'
+        let nextTurn = attacked
 
         if (actualAttack > actualDefense) {
-            attacked.life -= actualAttack
-            this.log.addMessage(`${attacking.name} causou ${actualAttack.toFixed(2)}
-        de dano em ${attacked.name}`)
-        }else{
-            this.log.addMessage(`${attacking.name} conseguiu defender o ataque ${actualAttack.toFixed(2)}
-        de dano em ${attacked.name}`)
+            let damage = actualAttack - actualDefense
+            attacked.life -= damage
+            this.log.addMessage(`**${attacking.name}** causou **${damage.toFixed(2)}** de dano em **${attacked.name}**.`, msgClass)
 
+            if (actualAttack < 1) {
+                this.log.addMessage(`**SISTEMA**: Ataque fraco (${actualAttack.toFixed(2)}). **${attacking.name}** ganhou um turno extra!`, 'system-msg')
+                nextTurn = attacking
+            } else {
+                nextTurn = attacked
+            }
+
+        } else {
+            let recoveredLife = 1 + (Math.random() * 2) 
+            attacked.life += recoveredLife > (attacked.maxlife - attacked.life) ? (attacked.maxlife - attacked.life) : recoveredLife
+            
+            this.log.addMessage(`**${attacked.name}** conseguiu defender o ataque de **${attacking.name}**!`, 'system-msg')
+            this.log.addMessage(`**SISTEMA**: **${attacked.name}** recuperou **${recoveredLife.toFixed(2)}** de vida por ter defendido.`, 'system-msg')
+            
+            nextTurn = attacked
         }
+        
+        this.turn = nextTurn
         this.update()
+
+        if (attacked.life <= 0) {
+            this.log.addMessage(`**SISTEMA**: **${attacked.name}** foi derrotado! **${attacking.name}** VENCEU!`, 'system-msg')
+            // Mantém o turno no vencedor para permitir "atacar o cachorro morto"
+            this.turn = attacking
+            this.update()
+        }
     }
         
+    update() {
+        // Fighter 1
+        this.fighter1EL.querySelector(".name").innerHTML = `
+            ${this.fighter1.name} - ${this.fighter1.lifeFormatted} HP (A: ${this.fighter1.attack} | D: ${this.fighter1.defense})` 
 
+        let f1pct = (this.fighter1.life / this.fighter1.maxlife) * 100
+        this.fighter1EL.querySelector(".bar").style.width = `${f1pct}%`
+        this.fighter1EL.querySelector(".char-img").src = this.fighter1.image
+        
+        this.fighter1EL.querySelector(".attackbutton").disabled = (this.turn !== this.fighter1) || (this.fighter1.life <= 0)
 
-update() {
-    // fighter1
-    this.fighter1EL.querySelector(".name").innerHTML = `
-        ${this.fighter1.name} - ${this.fighter1.life} HP`
+        // Fighter 2
+        this.fighter2EL.querySelector(".name").innerHTML = `
+            ${this.fighter2.name} - ${this.fighter2.lifeFormatted} HP (A: ${this.fighter2.attack} | D: ${this.fighter2.defense})` 
 
-    let f1pct = (this.fighter1.life / this.fighter1.maxlife) * 100
-    this.fighter1EL.querySelector(".bar").style.width = `${f1pct}%`
-
-
-    // fighter2
-    this.fighter2EL.querySelector(".name").innerHTML = `
-        ${this.fighter2.name} - ${this.fighter2.life} HP`
-
-    let f2pct = (this.fighter2.life / this.fighter2.maxlife) * 100
-    this.fighter2EL.querySelector(".bar").style.width = `${f2pct}%`
+        let f2pct = (this.fighter2.life / this.fighter2.maxlife) * 100
+        this.fighter2EL.querySelector(".bar").style.width = `${f2pct}%`
+        this.fighter2EL.querySelector(".char-img").src = this.fighter2.image
+        
+        this.fighter2EL.querySelector(".attackbutton").disabled = (this.turn !== this.fighter2) || (this.fighter2.life <= 0)
+    }
 }
 
-}
-
-class Log{
-    list =[]
-    constructor(listEL){
+class Log {
+    list = []
+    constructor(listEL) {
         this.listEL = listEL
     }
-    addMessage(msg){
-        this.list.push(msg)
+    addMessage(msg, className='') {
+        if (this.list.length >= 20) {
+            this.list.shift()
+        }
+        this.list.push({ msg, className }) 
         this.render()
     }
-    render(){
-        this.listEL.innerHTML=""
+    render() {
+        this.listEL.innerHTML = ""
 
-        for(let i in this.list){
-            this.listEL.innerHTML +=`<li> ${this.list[i]} </li>`
+        for(let item of this.list) {
+            this.listEL.innerHTML += `<li class="${item.className}"> ${item.msg} </li>`
         }
+        this.listEL.scrollTop = this.listEL.scrollHeight 
     }
 }
--- p2/aula_10/11/25/assests/JS/scripts.js
+++ p2/aula_10/11/25/assests/JS/scripts.js
@@ -1,14 +1,47 @@
-let char = new Knight('Meliodas')
-let monster = new (LittleMonster)
-let log = new Log(document.querySelector(".log"))
+document.addEventListener('DOMContentLoaded', () => {
+    const startButton = document.querySelector('#start-fight-button')
+    const selectElement = document.querySelector('#hero-select')
+    const charArea = document.querySelector('#char')
+    const monsterArea = document.querySelector('#monster')
+    const logArea = document.querySelector(".log")
 
+    // Esconde a área de luta e o log até o início do jogo
+    charArea.style.display = 'none'
+    monsterArea.style.display = 'none'
+    logArea.style.display = 'none'
 
-const stage = new Stage(
-    char,
-    monster,
-    document.querySelector('#char'),
-    document.querySelector('#monster'),
-    log)
-    
+    startButton.addEventListener('click', () => {
+        const selectedHero = selectElement.value
 
-    stage.start()
+        let char
+        if (selectedHero === 'knight') {
+            char = new Knight('Aragorn') // Nome de exemplo
+        } else if (selectedHero === 'sorcerer') {
+            char = new Sorcerer('Gandalf') // Nome de exemplo
+        } else {
+            alert('Por favor, selecione um herói.')
+            return
+        }
+
+        // Instancia o monstro e o log
+        let monster = new BigMonster() // Usando BigMonster
+        let log = new Log(logArea)
+
+        // Esconde o seletor e mostra a área de luta
+        document.querySelector('.selection-area').style.display = 'none'
+        charArea.style.display = 'block'
+        monsterArea.style.display = 'block'
+        logArea.style.display = 'block'
+        
+        // Inicia o Stage
+        const stage = new Stage(
+            char,
+            monster,
+            charArea,
+            monsterArea,
+            log)
+
+        stage.log.addMessage(`**SISTEMA**: A batalha começou! **${char.name}** vs **${monster.name}**!`, 'system-msg')
+        stage.start()
+    })
+})
